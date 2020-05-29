0de78fcd8cba6cbb0fead86767e4c71b44715e42
midoalone <mido_alone2001@yahoo.com>
2020-05-29 11:27:27 +0200

    Working

--- app/Ship/Parents/Transformers/Transformer.php
+++ app/Ship/Parents/Transformers/Transformer.php
@@ -12,44 +12,41 @@ use Apiato\Core\Abstracts\Transformers\Transformer as AbstractTransformer;
 abstract class Transformer extends AbstractTransformer {
 
   public function withMedia( $entity, $response, $name = "logo" ) {
-    public
-    function withMedia( $entity, $response, $name = "logo" ) {
 
-      $medias = $entity->getMedia( $name );
-      if ( $medias ) {
-        $url = [];
-        foreach ( $medias as $media ) {
-          $mediaUrl = $media->getUrl();
+    $medias = $entity->getMedia( $name );
+    if ( $medias ) {
+      $url = [];
+      foreach ( $medias as $media ) {
+        $mediaUrl = $media->getUrl();
 
-          if ( strpos( $mediaUrl, "http" ) === false ) {
-            $mediaUrl = "https://{$media->getUrl()}";
-          }
-
-          $url[] = str_replace( "/app/", "/", $mediaUrl );
+        if ( strpos( $mediaUrl, "http" ) === false ) {
+          $mediaUrl = "https://{$media->getUrl()}";
         }
 
-        if ( count( $url ) === 1 ) {
-          $url = $url[0];
-        }
-
-        $response[ $name ] = $url;
+        $url[] = str_replace( "/app/", "/", $mediaUrl );
       }
 
-      return $response;
-    }
-
-    public
-    function fillables( $entity, $response ) {
-      // Get values of fillables
-      $fillabeValues = [];
-      $fillables     = $entity->getFillable();
-
-      foreach ( $fillables as $fillable ) {
-        $fillabeValues[ $fillable ] = $entity->{$fillable};
+      if ( count( $url ) === 1 ) {
+        $url = $url[0];
       }
 
-      $response = array_merge( $response, $fillabeValues );
-
-      return $response;
+      $response[ $name ] = $url;
     }
+
+    return $response;
   }
+
+  public function fillables( $entity, $response ) {
+    // Get values of fillables
+    $fillabeValues = [];
+    $fillables     = $entity->getFillable();
+
+    foreach ( $fillables as $fillable ) {
+      $fillabeValues[ $fillable ] = $entity->{$fillable};
+    }
+
+    $response = array_merge( $response, $fillabeValues );
+
+    return $response;
+  }
+}
